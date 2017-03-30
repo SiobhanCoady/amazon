fc1a458ea52d6be2eb7666ac1044cf430f654af9
Siobhan Coady <siobhan.coady@gmail.com>
2017-03-30 15:05:22 -0700

    Add ability to add/update/delete new products

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
   get('/contact', { to: 'contact#index' })
   post('/contact', { to: 'contact#create', as: 'contact_submit' })
 
+  resources :products
+  # get('/products/new', { to: 'products#new', as: 'new_product' })
+  # get('/products', { to: 'products#index' })
+  # post('/products', { to: 'products#create' })
+  # get('/products/:id', { to: 'products#show', as: 'product' })
+  # delete('/products/:id', { to: 'products#destroy' })
+  # get('/products/:id/edit', { to: 'products#edit', as: 'edit_product' })
+  # patch('/products/:id', { to: 'products#update' })
+
+
   root 'welcome#index'
 
 end

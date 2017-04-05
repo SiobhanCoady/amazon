315de8cb05bac0aa59fb3ed7484134c3104fc02a
Jason <cameljeet@gmail.com>
2017-04-04 22:01:10 -0700

    Update db seeds to include user

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,34 @@ Category.create(
   ]
 )
 
+25.times do
+  User.create first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              email: Faker::Internet.email,
+              password: '12345678',
+              password_confirmation: '12345678'
+end
+puts Cowsay.say 'Created 100 users', :dragon
 
 100.times do
   category = Category.all.sample
+  user = User.all.sample
   Product.create title: Faker::Commerce.product_name,
                   description: Faker::Hipster.sentence,
                   price: Faker::Commerce.price,
-                  category_id: category.id
+                  category_id: category.id,
+                  user_id: user.id
 end
 
 products = Product.all
 
 products.each do |p|
   rand(0..10).times do
+    user = User.all.sample
     p.reviews.create({
       body: Faker::Friends.quote,
-      rating: rand(1..5)
+      rating: rand(1..5),
+      user_id: user.id
       })
   end
 end
@@ -33,13 +45,3 @@ reviews_count = Review.count
 
 puts Cowsay.say 'Created 100 products', :elephant
 puts Cowsay.say "Created #{reviews_count} reviews", :stegosaurus
-
-
-
-100.times do
-  User.create first_name: Faker::Name.first_name,
-              last_name: Faker::Name.last_name,
-              email: Faker::Internet.email
-end
-
-puts Cowsay.say 'Created 100 users', :dragon

91774f09353ca481428976ceb2fd1e4620c71a7d
Siobhan Coady <siobhan.coady@gmail.com>
2017-05-04 11:13:49 -0700

    Update API show.json.jbuilder

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   before_action :authenticate_user
 
   def index
@@ -9,10 +10,31 @@ class Api::V1::ProductsController < ApplicationController
     @product = Product.find params[:id]
   end
 
+  def create
+    @product = Product.new product_params
+    @product.user = @user
+
+    if @product.save
+      render json: "Product created!"
+    else
+      render json: "There was an error"
+    end
+  end
+
   private
 
   def authenticate_user
     @user = User.find_by_api_token params[:api_token]
     head :unauthorized if @user.nil?
+  end
+
+  def product_params
+    params.require(:product).permit([ :title,
+                                      :description,
+                                      :price,
+                                      :sale_price,
+                                      :category_id,
+                                      { tag_ids: [] }
+                                    ])
   end
 end
--- app/views/api/v1/products/show.json.jbuilder
+++ app/views/api/v1/products/show.json.jbuilder
@@ -10,11 +10,15 @@ json.favourites @product.favourites.count
 json.seller do
     json.first_name @product.user.first_name
     json.last_name @product.user.last_name
-  end
+end
 json.created_on @product.created_at.strftime('%Y-%B-%d')
 json.reviews @product.reviews do |review|
   json.id review.id
   json.body review.body
   json.rating review.rating
+  json.reviewer do
+      json.first_name review.user.first_name
+      json.last_name review.user.last_name
+  end
   json.likes review.likes.count
 end
